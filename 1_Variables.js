377443be1c341f79181b1f454ae5e68a6e8a46c3
Daniel Zamora Paez <[email>
2025-07-10 16:03:52 -0500

    Subida del ejercicio 1 del taller sobre JavaScript

---
+++ 1_Variables.js
@@ -0,0 +1,15 @@
+const PromptSync = require("prompt-sync");
+
+//Ejercicio 1
+let nombre = "Daniel";
+let edad = 22;
+let esEstudiante = true;
+
+let sinValor;
+let valorNulo = null;
+
+console.log(nombre);
+console.log(edad);
+console.log(esEstudiante);
+console.log(sinValor);
+console.log(valorNulo);
